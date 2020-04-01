eabee361fc5946e767a628dbbb403a3cc57a5054
mikoyanhsch <55558409+mikoyanhsch@users.noreply.github.com>
2020-03-31 23:17:15 -0500

    Add SQL Query Ability

--- src/main/java/basketballstats/playerFill.java
+++ src/main/java/basketballstats/playerFill.java
@@ -23,6 +23,10 @@ public class playerFill extends HttpServlet {
     private final CloseableHttpClient httpClient = HttpClients.createDefault();
     private Players[] players;
 
+    private void close() throws IOException {
+        httpClient.close();
+    }
+
     public void doGet (HttpServletRequest req, HttpServletResponse res) throws IOException {
         // GoogleSQL Connection Setup
         String instance = "basketball-db";
@@ -31,6 +35,9 @@ public class playerFill extends HttpServlet {
         String pass="Sr4*8DNgZbvHqnee";
         String ip="104.154.138.136";
         String host = "jdbc:mysql://" + ip + ":3306/" + db;
+
+        String query = "insert into players(first_name, last_name, position, height_feet, height_inches," +
+                " weight_pounds, team_id) VALUES(?,?,?,?,?,?,?)";
 
         try {
             // Attempt connection to SQL server via JDBC. Ironically this does not require forName import (Deprecated)
@@ -42,15 +49,34 @@ public class playerFill extends HttpServlet {
             while(r.next()) {
                 System.out.println(r.getString(1));
             }
+            PreparedStatement ps;
 
             for (int i = 1; i <= 33; i++) {
                 apiRequest("https://www.balldontlie.io/api/v1/players?per_page=100&page=" + i);
+                for (Players p : players) {
+                    ps = c.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+
+                    ps.setString(1, p.getFirstName());
+                    ps.setString(2, p.getLastName());
+                    ps.setString(3, p.getPosition());
+                    ps.setInt(4, p.getHeightFeet());
+                    ps.setInt(5, p.getHeightInches());
+                    ps.setInt(6, p.getWeight());
+                    ps.setInt(7, p.getId());
+
+                    ps.executeUpdate();
+                    c.commit();
+                }
             }
 
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+            close();
+        }
+        res.sendRedirect("/players.jsp");
     }
 
     private void apiRequest(String url) throws Exception {
